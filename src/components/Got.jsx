1b1b6d7e8ce59f6b930e9dd8e60d8b0f96b3cef8
Joseph Caruana <jcaruana614@gmail.com>
2020-06-04 21:09:52 -0700

    game of thrones working

--- src/components/Got.jsx
+++ src/components/Got.jsx
@@ -5,25 +5,29 @@ import Navbar from './Navbar';
 const Got = (props) => {
     const [characters, setCharacters] = useState([]);
     function getGot() {
-        axios.get("https://anapioficeandfire.com/api/houses")
+        axios.get("https://anapioficeandfire.com/api/characters/823")
             .then(res => {
                 console.log(res)
-                setCharacters([...res.data])
+                setCharacters(res.data)
             })
             .catch(err => console.log(err))
     }
-    
+
     return (
         <div>
             <Navbar />
             <h1>Winter is coming</h1>
             <button onClick={getGot}>Search GOT!</button>
-            <ul>
+            {/* <ul>
             {characters.map((character, index) => 
                 <li key={index}>{character.name}</li>
       )}
-            </ul>
-
+            </ul> */}
+            <h1>{characters.name}</h1>
+            <h1>{characters.culture}</h1>
+            <h1>{characters.born}</h1>
+            <p>{characters.titles}</p>
+            <p>{characters.aliases}</p>
 
         </div>
     );

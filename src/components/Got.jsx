7726416cac21f169802e3826c942ab333ea26a1f
Joseph Caruana <jcaruana614@gmail.com>
2021-04-11 14:37:27 -0700

    git ignore

--- src/components/Got.jsx
+++ src/components/Got.jsx
@@ -3,34 +3,34 @@ import axios from 'axios';
 import Navbar from './Navbar';
 
 const Got = (props) => {
-    const [characters, setCharacters] = useState([]);
-    function getGot() {
-        axios.get("https://anapioficeandfire.com/api/characters/823")
-            .then(res => {
-                console.log(res)
-                setCharacters(res.data)
-            })
-            .catch(err => console.log(err))
-    }
+	const [characters, setCharacters] = useState([]);
+	function getGot() {
+		axios
+			.get('https://anapioficeandfire.com/api/characters/823')
+			.then((res) => {
+				console.log(res);
+				setCharacters(res.data);
+			})
+			.catch((err) => console.log(err));
+	}
 
-    return (
-        <div>
-            <Navbar />
-            <h1>Winter is coming</h1>
-            <button onClick={getGot}>Search GOT!</button>
-            {/* <ul>
+	return (
+		<div>
+			<Navbar />
+			<h1>Winter is coming</h1>
+			<button onClick={getGot}>Search GOT!</button>
+			{/* <ul>
             {characters.map((character, index) => 
                 <li key={index}>{character.name}</li>
       )}
             </ul> */}
-            <h1>{characters.name}</h1>
-            <h1>{characters.culture}</h1>
-            <h1>{characters.born}</h1>
-            <p>{characters.titles}</p>
-            <p>{characters.aliases}</p>
-
-        </div>
-    );
-}
+			<h1>{characters.name}</h1>
+			<h1>{characters.culture}</h1>
+			<h1>{characters.born}</h1>
+			<p>{characters.titles}</p>
+			<p>{characters.aliases}</p>
+		</div>
+	);
+};
 
 export default Got;
